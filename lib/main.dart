cedc81e3239bbe6acf9802093fc1b531b89e8820
mdmojidulkhan35-art <mdmojidulkhan35@gmail.com>
2026-09-11 22:27:45 +0600

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+import 'screens/auth_gate.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const BondhuApp());
+}
+
+class BondhuApp extends StatelessWidget {
+  const BondhuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bondhu',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const AuthGate(),
+    );
+  }
+}
